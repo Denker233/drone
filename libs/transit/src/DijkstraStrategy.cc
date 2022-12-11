618389a9094440f2a6b787d20923fef7693d640c
Minrui Tian <tian0138@umn.edu>
2022-12-10 21:12:05 -0600

    individual strategy

--- libs/transit/src/DijkstraStrategy.cc
+++ libs/transit/src/DijkstraStrategy.cc
@@ -51,3 +51,50 @@ void DijkstraStrategy::Move(IEntity* entity, double dt){
     }
 }
 
+DijkstraStrategy::decision(IEntity* entity, std::vector< IStrategy*> s2){ //loop through every station and pick shortest strategy
+            IStrategy MinStrategy = new ;
+            int i=0;
+            for(EachStrategy:s2){
+            if(i==0){
+                MinStrategy=EachStrategy;
+            }
+            if(EachStrategy->TimeSwap(entity,midV3)<MinStrategy->TimeSwap(entity,midV3)){
+                MinStrategy=EachStrategy;
+            }
+            }
+            if(MinStrategy->TimeSwap(entity,midV3)>=this->TimeDirect(entity)){
+            return this;
+            }
+            return MinStrategy;
+}
+
+DijkstraStrategy::Distance(IEntity* entity){
+            Vector3 currentPos = entity->GetPosition();
+            float TotalDistance=0;
+            for(int i =0; i< maxIndex;i++){
+                TotalDistance += PointDistance(Vector3(path[i].at(0), path[i].at(1), path[i].at(2)),Vector3(path[i+1].at(0), path[i+1].at(1), path[i+1].at(2)))
+            }
+            return TotalDistance;
+        }
+
+DijkstraStrategy::TimeDirect(IEntity* entity){
+            if(entity->GetBattery()>this->Distance(entity)){
+                return this->Distance(entity)/entity->GetSpeed();
+            }
+            else{
+                return entity->GetBattery()/entity->GetSpeed()+(this->Distance(entity)-entity->GetBattery())/(entity->GetSpeed()/2);
+            }
+        }
+DijkstraStrategy::TimeSwap(IEntity* entity,Vector3* SwapStation){
+            Vector3 currentPos = entity->GetPosition();
+            float DistanceToSwap = PointsDistance(currentPos,SwapStation);
+            if(entity->GetBattery()>DistanceToSwap){
+                return this->Distance(entity)/entity->GetSpeed();
+            }
+            else if (entity->GetBattery()){
+                return entity->GetBattery()/entity->GetSpeed()+(DistanceToSwap-entity->GetBattery())/(entity->GetSpeed()/2)+(this->Distance(entity)-DistanceToSwap)/(entity->GetSpeed());
+            }
+            else{
+                return DistanceToSwap/entity->GetSpeed()+(this->Distance(entity)-DistanceToSwap)/(entity->GetSpeed()*2);
+            }
+}        
