b56c132be81a1896042a5aa35fbb081f0c755c43
Minrui Tian <tian0138@umn.edu>
2022-12-13 17:01:46 -0600

    pure

--- libs/transit/include/Drone.h
+++ libs/transit/include/Drone.h
@@ -38,10 +38,10 @@ class Drone : public IEntity {
     // std::string s(type);
     return type;}
 
-  void GetNearestEntity(std::vector<IEntity*> scheduler);
+  void GetNearestEntity(std::vector<IEntity*> scheduler,std::vector<IEntity*> stations);
 
   // Updates the drone's position
-  void Update(double dt, std::vector<IEntity*> scheduler);
+  void Update_Drone(double dt, std::vector<IEntity*> scheduler,std::vector<IEntity*> stations);
 
   void SetPosition(Vector3 pos_) { position = pos_; }
 
